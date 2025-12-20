aaf8292659307c0bb3aee40348bcc51d63e841d9
Quang <ngocquangntth@gmail.com>
2025-12-20 21:10:51 +0700

    Router update

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import CarDetail from "./components/Car Detail/CarDetail";
+import { Route, Routes } from "react-router";
 
 function App() {
   return (
     <>
       <div className="app-container">
         <Header />
-        {/* <Home /> */}
-        <CarDetail />
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="car-detail" element={<CarDetail />} />
+        </Routes>
         <Footer />
       </div>
     </>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,18 +3,25 @@ import "./Header.css";
 import logoImg from "../img/cbd16f02ecd93bbc4d7b2ad92d273e350353d94e.png";
 import Vector from "../LOGO/Vector";
 import SignUp from "../LOGO/SignUp";
+import { Link } from "react-router";
 function Header() {
   return (
     <header className="header-container">
-      <div className="header-logo-container">
-        <img
-          style={{ width: "117.63px", height: "32px" }}
-          src={logoImg}
-          alt=""
-        />
-      </div>
+      <Link className="link-style" to={""}>
+        <div className="header-logo-container">
+          <img
+            style={{ width: "117.63px", height: "32px" }}
+            src={logoImg}
+            alt=""
+          />
+        </div>
+      </Link>
+
       <div className="header-link-container">
-        <div className="header-element">New Cars</div>
+        <Link className="link-style " to={"/car-detail"}>
+          <div className="header-element">New Cars</div>
+        </Link>
+
         <div className="header-element">Used Cars</div>
         <div className="header-element">Compare</div>
         <div className="header-element">Sell</div>
